2c3c55bd728c98a07d99ccf66f3d51fe19948f57
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2017-01-28 10:35:52 +0700

    Add Seeding option

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Migrate extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
     public function index()
     {
         $this->load->library('migration');
@@ -10,5 +16,20 @@ class Migrate extends CI_Controller
         if (!$this->migration->current()) {
             show_error($this->migration->error_string());
         }
+    }
+
+    public function seed()
+    {
+        $this->load->model('admin_model');
+
+        $out = '';
+
+        if (!$this->admin_model->find(1)) {
+            $this->admin_model->add('admin', 'admin@example.com', 'admin', 1);
+            $out .= "First Admin Created. \n";
+        }
+
+        $out .= "Seeded. \n";
+        $this->output->set_output($out);
     }
 }
