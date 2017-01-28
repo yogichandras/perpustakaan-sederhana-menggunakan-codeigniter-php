4b9465b7352fe1c22fe917d1769b0a169b36c55a
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2017-01-28 10:25:17 +0700

    Update migration version

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20170125063524;
+$config['migration_version'] = 20170127091542;
 
 /*
 |--------------------------------------------------------------------------
