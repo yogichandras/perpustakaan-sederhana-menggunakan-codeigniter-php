0a8f5f6483923ed23efec6160490f307aa792716
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2017-02-02 22:57:11 +0700

    Initialize site_lang.php

---
+++ application/language/english/site_lang.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['site_name'] = 'Library System';
+$lang['Search for...'] = 'Search for...';
+$lang['Catalog'] = 'Catalog';
+$lang['Members'] = 'Members';
+$lang['Settings'] = 'Settings';
+$lang['Logout'] = 'Logout';
+$lang['Login'] = 'Login';
+$lang['Email'] = 'Email';
+$lang['Password'] = 'Password';
+$lang['Email or Password is invalid'] = 'Email or Password is invalid';
+$lang['Error connection'] = 'Error connection';
